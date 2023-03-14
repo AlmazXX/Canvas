f2fb54a8aee2788c368134f256676db25343d8bd
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-03-14 16:19:33 +0600

    feat: Added switch processing of coming msgs

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import { randomUUID } from "crypto";
 import express from "express";
 import expressWs from "express-ws";
-import { ActiveConnections } from "./types";
+import { ActiveConnections, Pixel } from "./types";
 
 const port = 8000;
 const app = express();
@@ -12,12 +12,31 @@ app.use(cors());
 const router = express.Router();
 
 const activeConnections: ActiveConnections = {};
+let pixels: Pixel[] = [];
 
-router.ws("/canvas", (ws, _req) => {
+router.ws("/canvas", (ws) => {
   const id = randomUUID();
-  console.log("Client connected! id =", id);
-
+  console.log("client connected! id =", id);
   activeConnections[id] = ws;
+
+  ws.send(JSON.stringify({ type: "INIT", data: pixels }));
+
+  ws.on("message", (msg) => {
+    const data = JSON.parse(msg.toString());
+
+    switch (data.type) {
+      case "DRAW":
+        pixels.push(...data.data);
+
+        Object.keys(activeConnections).forEach((id) => {
+          const conn = activeConnections[id];
+          conn.send(JSON.stringify({ type: "DRAW", data }));
+        });
+        break;
+      default:
+        break;
+    }
+  });
 
   ws.on("close", () => {
     console.log("Client disconnected! id =", id);
